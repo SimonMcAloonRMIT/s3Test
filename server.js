482ac8c44468c9a200c64fff68350da35902f208
Simon McAloon <simonmcaloon@gmail.com>
2018-12-23 03:40:18 +1100

    API to get files direct from s3 for PDF generation

--- server.js
+++ server.js
@@ -26,80 +26,100 @@ app.get('/', function (req, res) {
 app.use('/cache', express.static(__dirname + '/cache'));
 
  // getImageFromCache
-//  app.get('/getImageFromCache', function(req, res) {
+ app.get('/getImageFromCache', function(req, res) {
 
-//     var filename = req.query.img
-//     res.sendFile(__dirname + "/cache/" + filename);
-// });
+    var filename = req.query.img
+    res.sendFile(__dirname + "/cache/" + filename);
+});
+
+ // getImageFromS3
+ app.get('/getImageFromS3', function(req, res) {
+
+    var filename = req.query.img
+    var s3 = new AWS.S3();
+
+    var options = {
+        Bucket: 'smc-bucket1',
+        Key: filename,
+    };
+
+    s3.getObject(options)
+    .createReadStream()
+    .pipe(res)
+    .on('finish', function() {
+        util.log('download competed.');
+    });    
+
+});
 
  app.get('/getFiles', function (req, res) {
     util.log('-------------');
-    util.log('Getting files from s3');
+    //util.log('Getting files from s3');
 
     // Test data
     // ---------
     // generate uuid
-    var uuid = uuidv4() + ".";
+    // var uuid = uuidv4() + ".";
 
-    console.log('DOWNLOADS STARTED...');
+    // console.log('DOWNLOADS STARTED...');
 
-    var s3 = new AWS.S3();
+    // var s3 = new AWS.S3();
 
-    var getObject = function(imageNmae) {
-        return new Promise(function(success, reject) {
-            s3.getObject(
-                { Bucket: "smc-bucket1", Key: imageNmae },
-                function (error, data) {
-                    if(error) {
-                        reject(error);
-                    } else {
-                        data.Filename = imageNmae;
-                        //console.log(data);
-                        success(data);
-                        util.log(imageNmae + ' - file downloaded');
-                    }
-                }
-            );
-        });
-    }
+    // var getObject = function(imageNmae) {
+    //     return new Promise(function(success, reject) {
+    //         s3.getObject(
+    //             { Bucket: "smc-bucket1", Key: imageNmae },
+    //             function (error, data) {
+    //                 if(error) {
+    //                     reject(error);
+    //                 } else {
+    //                     data.Filename = imageNmae;
+    //                     //console.log(data);
+    //                     success(data);
+    //                     util.log(imageNmae + ' - file downloaded');
+    //                 }
+    //             }
+    //         );
+    //     });
+    // }
     
-    var promises = [];
-    var fileContentList = new ArrayList();
+    //var promises = [];
+    //var fileContentList = new ArrayList();
     var fileNameList = new ArrayList();
 
     fileNameList.add(['image0.jpg','image1.gif','image2.jpg','image3.jpg','image4.jpg','image5.jpg','image6.jpg','image7.png','image8.png','image9.jpg','image10.jpg','image11.jpg','image12.jpg','image13.jpg']);
     
-    for(var i = 0; i < fileNameList.length; i++){
-        promises.push(getObject(fileNameList.get(i)));
-        util.log('getting image - ' + fileNameList.get(i));
-    }
+    // for(var i = 0; i < fileNameList.length; i++){
+    //     promises.push(getObject(fileNameList.get(i)));
+    //     util.log('getting image - ' + fileNameList.get(i));
+    // }
     
-    Promise.all(promises)
-    .then(function(results) {
-        for(var index in results) {
-            var data = results[index];
-            fileContentList.add(data);
-            //console.log(results[index]);
-            //console.log(data.Body);
-        }
+    // Promise.all(promises)
+    // .then(function(results) {
+    //     for(var index in results) {
+    //         var data = results[index];
+    //         fileContentList.add(data);
+    //         //console.log(results[index]);
+    //         //console.log(data.Body);
+    //     }
 
-        // continue your process here
-        util.log('Promise DONE!');
-        console.log('DOWNLOADS DONE!');
+    //     // continue your process here
+    //     util.log('Promise DONE!');
+    //     console.log('DOWNLOADS DONE!');
 
-        console.log('WRITING FILES STARTED...');
-        for(var i = 0; i < fileContentList.length; i++ ) {
-            //console.log(fileContentList.get(i));
+    //     console.log('WRITING FILES STARTED...');
+    //     for(var i = 0; i < fileContentList.length; i++ ) {
+    //         //console.log(fileContentList.get(i));
 
-            var filenameArr = fileContentList.get(i).Filename.split(".");
-            var filename = filenameArr[0] + "-" + uuid + filenameArr[1];
+    //         var filenameArr = fileContentList.get(i).Filename.split(".");
+    //         var filename = filenameArr[0] + "-" + uuid + filenameArr[1];
 
-            var path = 'cache/' + filename;
+    //         var path = 'cache/' + filename;
          
-            fs = require('fs');
-            fs.writeFileSync(path, fileContentList.get(i).Body);        
-        }
-        console.log('WRITING FILES DONE!');
+    //         fs = require('fs');
+    //         fs.writeFileSync(path, fileContentList.get(i).Body);        
+    //     }
+    //     console.log('WRITING FILES DONE!');
 
         util.log('ready to create PDF');
 
@@ -107,15 +127,24 @@ app.use('/cache', express.static(__dirname + '/cache'));
         // -----------
         var content = "<h2>s3 private file cache test</h2><hr />";
 
-        for(var i = 0; i < fileContentList.length; i++) {
+        // for(var i = 0; i < fileContentList.length; i++) {
 
-            var filenameArr = fileContentList.get(i).Filename.split(".");
-            var filename = filenameArr[0] + "-" + uuid + filenameArr[1];
+        //     var filenameArr = fileContentList.get(i).Filename.split(".");
+        //     var filename = filenameArr[0] + "-" + uuid + filenameArr[1];
 
-            content += "<div>" + fileContentList.get(i).Filename + "</div>";
-            //content += "<img style='width: 200px;' src='http://localhost:3002/getImageFromCache?img=" + filename + "' /><br /><br />"; // using API
-            content += "<img style='width: 200px;' src='http://localhost:3003/cache/" + filename + "' /><br /><br />"; // using static location
-        }
+        //     content += "<div>" + fileContentList.get(i).Filename + "</div>";
+        //     content += "<img style='width: 200px;' src='http://localhost:3003/getImageFromCache?img=" + fileContentList.get(i).Filename + "' /><br /><br />"; // using API
+        //     //content += "<img style='width: 200px;' src='http://localhost:3003/getImageFromCache?img=" + filename + "' /><br /><br />"; // using API
+        //     //content += "<img style='width: 200px;' src='http://localhost:3003/cache/" + filename + "' /><br /><br />"; // using static location
+        // }
+
+        for(var i = 0; i < fileNameList.length; i++) {
+
+            content += "<div>" + fileNameList.get(i) + "</div>";
+            content += "<img style='width: 200px;' src='http://localhost:3003/getImageFromS3?img=" + fileNameList.get(i) + "' /><br /><br />"; // using API
+            //content += "<img style='width: 200px;' src='http://localhost:3003/getImageFromCache?img=" + filename + "' /><br /><br />"; // using API
+            //content += "<img style='width: 200px;' src='http://localhost:3003/cache/" + filename + "' /><br /><br />"; // using static location
+        }        
 
         var html = content;
         var finalOptions = "";
@@ -127,25 +156,25 @@ app.use('/cache', express.static(__dirname + '/cache'));
             util.log('\x1b[32m%s\x1b[0m', 'PDF generated OK! - and returned');  
 
             // clean up filess
-            util.log('Deleting cache files');
-            for(var i = 0; i < fileContentList.length; i++) {
+            // util.log('Deleting cache files');
+            // for(var i = 0; i < fileContentList.length; i++) {
 
-                var filenameArr = fileContentList.get(i).Filename.split(".");
-                var filename = filenameArr[0] + "-" + uuid + filenameArr[1]; 
+            //     var filenameArr = fileContentList.get(i).Filename.split(".");
+            //     var filename = filenameArr[0] + "-" + uuid + filenameArr[1]; 
 
-                fs.unlink(__dirname + "/cache/" + filename, function (err) {
-                    if (err) throw err;
-                    // if no error, file has been deleted successfully
-                    //console.log('File deleted!');
-                }); 
-            }    
+            //     fs.unlink(__dirname + "/cache/" + filename, function (err) {
+            //         if (err) throw err;
+            //         // if no error, file has been deleted successfully
+            //         //console.log('File deleted!');
+            //     }); 
+            // }    
         });            
 
 
-    })
-    .catch(function(err) {
-        util.log(err);
-    });
+    // })
+    // .catch(function(err) {
+    //     util.log(err);
+    // });
 });
 
 // code from project
